edd4dc15e460c3884252e06d82ee1726e02fa37f
Vinicius do Prado Vieira <viniciusv@live.com>
2020-06-30 17:20:30 -0300

    Able to launch ball from paddle

--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -4,17 +4,18 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    [SerializeField] int SCREEN_WIDTH = 16;
-
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [SerializeField] int SCREEN_WIDTH_IN_UNITS = 16;
+    [SerializeField] float PADDLE_POS_MAX = 15f;
+    [SerializeField] float PADDLE_POS_MIN = 1f;
 
     // Update is called once per frame
     void Update()
     {
-        Debug.Log(Input.mousePosition.x / Screen.width * SCREEN_WIDTH);
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * SCREEN_WIDTH_IN_UNITS;
+        Vector2 paddlePos = new Vector2(mousePosInUnits, transform.position.y)
+        {
+            x = Mathf.Clamp(mousePosInUnits, PADDLE_POS_MIN, PADDLE_POS_MAX)
+        };
+        transform.position = paddlePos;
     }
 }
